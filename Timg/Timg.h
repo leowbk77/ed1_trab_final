59fe80ff9f4d4996edf2ac879ad6f2dd85efc239
Leonardo <leonardo.mf@ufu.br>
2021-06-10 22:54:27 -0300

    tad imagem adicionado

---
+++ Timg/Timg.h
@@ -0,0 +1,20 @@
+#ifndef _timgh_
+#define _timgh_
+
+/*
+*   DEFINES
+*/
+
+#define SUCCESS 0
+#define INVALID_NULL_POINTER -1
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
+#define ELEM_NOT_FOUND -4
+
+/*
+*   FUNCOES
+*/
+
+
+
+#endif
