dc5136b0f58746891a6468a86c85ea0ee3b80b5f
Lucas Ketelhut <lucas-kmk@hotmail.com>
2021-06-17 18:35:54 -0300

    TAD lista adicionado com struct ponto

---
+++ Tlist/Tlist.c
@@ -0,0 +1,119 @@
+#include "Tlist.h"
+#include <stdlib.h>
+
+typedef struct list_node node;
+
+struct list {
+    node *head;
+    node *end;
+};
+
+struct list_node {
+    ponto p;
+    node *next;
+};
+
+list *list_create() {
+    list *list; 
+    
+    list = malloc(sizeof(list));
+    if (list != NULL) {
+        list -> head = NULL;
+        list -> end = NULL;
+    }
+
+    return list;
+}
+
+int list_free(list *list) {
+    if (list == NULL) {
+        return INVALID_NULL_POINTER;
+    }
+
+    node *aux;
+    aux = list -> head;
+
+    while (aux != NULL) {
+        aux = aux -> next;
+
+        free(list -> head);
+
+        list -> head = aux;
+    }
+
+    free(list);
+    return SUCCESS;
+}
+
+int push(list *list, ponto ponto) {
+    if (list == NULL) {
+        return INVALID_NULL_POINTER;
+    }
+
+    node *node_data;
+    node_data = malloc(sizeof(node_data));
+    if (node_data == NULL) {
+        return OUT_OF_MEMORY;
+    }
+
+    node_data -> p = ponto; 
+    
+    if (list -> head == NULL) {
+        list -> head = node_data;
+        list -> end = node_data;
+        node_data -> next = NULL;
+
+        return SUCCESS;
+    }
+
+    (list -> end) -> next = node_data;
+    list -> end = node_data;
+    node_data -> next = NULL;
+
+    return SUCCESS;
+}
+
+int pop(list *list, ponto *p) {
+    if (list == NULL) {
+        return INVALID_NULL_POINTER;
+    }
+
+    if (list -> head == NULL) {
+        return ELEM_NOT_FOUND;
+    }
+
+    *p = (list -> end) -> p;
+
+    node *aux;
+    aux = list -> head;
+
+    if (aux -> next == NULL) {
+        free(aux);
+        list -> head = NULL;
+        list -> end = NULL;
+
+        return SUCCESS;
+    }
+
+    while (aux -> next != list -> end) {
+        aux = aux -> next;
+    }
+
+    aux -> next = NULL;
+    free(list -> end);
+    list -> end = aux;
+
+    return SUCCESS;
+}
+
+int is_empty(list *list) {
+    if (list == NULL) {
+        return INVALID_NULL_POINTER;
+    }
+
+    if (list -> head == NULL) {
+        return 1; // se estiver vazia, retorna 1:true
+    } else {
+        return 0; // se não estiver vazia, retorna 0:false
+    }
+}
---
+++ Tlist/Tlist.h
@@ -0,0 +1,36 @@
+#ifndef _tlisth_
+#define _tlisth_
+
+/*
+*   DEFINES
+*/
+
+#define SUCCESS 0
+#define INVALID_NULL_POINTER -1
+#define OUT_OF_MEMORY -2
+#define OUT_OF_RANGE -3
+#define ELEM_NOT_FOUND -4
+
+typedef struct list list;
+typedef struct ponto ponto;
+
+struct ponto {
+    int x;
+    int y;
+};
+
+/*
+*   FUNCOES
+*/
+
+list *list_create();
+
+int list_free(list *list);
+
+int push(list *list, ponto p);
+
+int pop(list *list, ponto *p);
+
+int is_empty(list *list);
+
+#endif
